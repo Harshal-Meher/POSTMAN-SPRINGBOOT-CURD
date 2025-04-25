8585369cead1022fb8234b8075c7f60d2f0e9bbe
Harshal-Meher <harshalmeher294@gmail.com>
2025-04-25 17:23:06 +0530

    Postman curd spring boot

---
+++ restapi/src/main/java/com/rssoft/restapi/dao/CourseDao.java
@@ -0,0 +1,1 @@
+package com.rssoft.restapi.dao;import com.rssoft.restapi.entities.Course;import org.springframework.data.jpa.repository.JpaRepository;public interface CourseDao extends JpaRepository<Course , Integer> {}
---
+++ restapi/src/main/java/com/rssoft/restapi/repositories/CourseRepo.java
@@ -0,0 +1,1 @@
+package com.rssoft.restapi.repositories;public class CourseRepo {}
